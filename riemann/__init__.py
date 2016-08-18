f5699e891a29f2bedd9845f119d8194ed55b99e5
openglx <openglx@users.noreply.github.com>
2016-08-18 11:43:11 +0100

    Introduce attributes, new example client
    Also updates protobuf protocol version to newer version.

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -6,7 +6,7 @@ class RiemannUDPTransport():
     def __init__(self, host='127.0.0.1', port=5555):
         self._host = host
         self._port = port
-    
+
     def write(self, buffer): # needs cleanup, conn pool
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(buffer, (self._host, self._port))
@@ -16,31 +16,31 @@ class RiemannTCPTransport():
     def __init__(self, host='127.0.0.1', port=5555):
         self._host = host
         self._port = port
-    
+
     def write(self, buffer):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self._host,self._port))
         l = len(buffer)
         hdr = struct.pack('!I', l)
         sock.send(hdr)
         sock.send(buffer)
-        r = sock.recv(4) 
+        r = sock.recv(4)
         hl = struct.unpack('!I', r)
         res = sock.recv(hl[0])
         c = pb.Msg().FromString(res)
-        sock.close() 
+        sock.close()
         return c
 
 class RiemannClient():
     """
-    Client to Riemann. 
+    Client to Riemann.
     RiemannClient constructor can receive the following parameters:
     - host
     - port
     - transport (RiemannUDPTransport, RiemannTCPTransport or any class that
       implements the write() method and receive both host and port on __init__())
 
-    Example: 
+    Example:
     from riemann import RiemannClient
 
     rc = RiemannClient()
@@ -59,8 +59,19 @@ class RiemannClient():
         ev = pb.Event()
         for k in self._fields:
             if edict.has_key(k): setattr(ev, k, edict[k]) 
+        if 'attributes' in edict:
+            attrs = []
+            for k in edict['attributes']:
+                attrib = pb.Attribute()
+                setattr(attrib, 'key', k)
+                setattr(attrib, 'value', edict['attributes'][k])
+                attrs.append(attrib)
+            print(repr(attrs))
+            ev.attributes.extend(attrs)
+            print(repr(ev))
         if 'tags' in edict:
             ev.tags.extend(edict['tags'])
+            print(repr(ev))
         if 'metric' in edict:
             ev.metric_f = float(edict['metric'])
         msg = pb.Msg()
@@ -72,3 +83,4 @@ class RiemannClient():
         msg = pb.Msg()
         msg.query.string = str(query)
         return self._transport.write(msg.SerializeToString())
+
